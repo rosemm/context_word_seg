bc980840fd8908a618112868bcd44fe824d9a979
Rose Maier <rosem@uoregon.edu>
2015-09-16 12:47:35 -0700

    init

---
+++ _OPEN_ME.R
@@ -0,0 +1,19 @@
+# These are the instructions for coding contexts in the Korman transcripts, using R! What fun.
+# This R window is the "editor". To work on the coding, you'll be working in the "console" window instead. It should have a > where you can click to move your cursor there.
+
+# Step 1: Load the functions
+# Copy-paste the following code to the console, and hit enter:
+source("coding_scripts.r")
+
+# Step 2: Begin coding!
+# Copy-paste the following code to the console, and hit enter:
+CodeContexts()
+# This will begin the coding program, which will ask you to type input right into the console window. If you have any questions, email Rose.
+# Sections of transcripts will appear on the screen, and after each one you will be asked to enter the context. 
+
+# Step 3: Finish coding!
+# After each transcript section, you have the option to quit (the program will ask if you want to code another one, and if you type n for no, then it will save and quit).
+# Whenever you want to stop, just finish that transcript section and then type "n" when it asks if you want to code another one. Your work will automatically be saved to the text file called "coding_doc.txt". 
+# Then quit R (command-Q). R will ask if you want to save a work space image - say no. Your coding isn't saved in R itself, it's all just in that coding_doc.txt file.
+
+# If you want to see what the coding_doc.txt file looks like, go ahead and open it, but BE VERY CAREFUL not to delete it, or edit it yourself. 
