a80f0b4a4204fb87a6aed55e09e5a9397d80bd73
Rose Maier <rosem@uoregon.edu>
2015-11-02 02:16:08 -0800

    added functionality to BlankDoc() for making the coding_doc_utt_KEY.txt file

--- coding_scripts.R
+++ coding_scripts.R
@@ -66,11 +66,11 @@ CleanTranscripts <- function(wd="./transcripts/"){
   return(transcripts)
 }
 
-BlankDoc <- function(wd="./transcripts/"){
+BlankDoc <- function(wd="./transcripts/", for.coding=TRUE){
   # write blank coding_doc.txt for coders to code from
   if(!require(dplyr)) install.packages("dplyr")
   
-  coding_doc <- data.frame(LineNum=NULL, UttNum=NULL, file=NULL)
+  coding_doc <- data.frame(LineNum=NULL, UttNum=NULL, file=NULL, utterance=NULL)
   
   transcripts <- CleanTranscripts()
   
@@ -78,29 +78,31 @@ BlankDoc <- function(wd="./transcripts/"){
   Nfiles <- length(files)
   
   for(f in 1:Nfiles){
-    this_coding_doc <- data.frame(dplyr::filter(dplyr::select(transcripts[[f]], LineNum, UttNum), !is.na(UttNum) ) )
+    this_coding_doc <- data.frame(dplyr::filter(dplyr::select(transcripts[[f]], LineNum, UttNum, utterance), !is.na(UttNum) ) )
     this_coding_doc$file <- files[f]
     coding_doc <- rbind(coding_doc, this_coding_doc)
   }
   
-  coding_doc$coder <- NA
-  coding_doc$date <- NA
-  coding_doc$context <- NA
-  
-  # each utterance should be coded multiple times if there is overlap between windows. 
-  # duplicate the coding rows for each time an utterance should be coded
-  coding_doc1 <- coding_doc
-  coding_doc1$pass <- 1
-  coding_doc2 <- coding_doc
-  coding_doc2$pass <- 2
-  coding_doc3 <- coding_doc
-  coding_doc3$pass <- 3
-  
-  coding_doc_file <- rbind(coding_doc1, coding_doc2, coding_doc3)
+  if(for.coding){
+    coding_doc$coder <- NA
+    coding_doc$date <- NA
+    coding_doc$context <- NA
+    # each utterance should be coded multiple times if there is overlap between windows. 
+    # duplicate the coding rows for each time an utterance should be coded
+    coding_doc1 <- coding_doc
+    coding_doc1$pass <- 1
+    coding_doc2 <- coding_doc
+    coding_doc2$pass <- 2
+    coding_doc3 <- coding_doc
+    coding_doc3$pass <- 3
+    
+    coding_doc <- rbind(coding_doc1, coding_doc2, coding_doc3)
+    coding_doc$utterance <- NULL # drop this column for coding purposes
+  }
 
-  return(coding_doc_file)
+  return(coding_doc)
 }
-# write.table(coding_doc_file, file="coding_doc.txt", quote=F, col.names=T, row.names=F, append=F, sep="\t")
+# write.table(coding_doc, file="coding_doc.txt", quote=F, col.names=T, row.names=F, append=F, sep="\t")
 
 
 CodeContexts <- function(this_pass=1, window_size=30, slide_by=10){
