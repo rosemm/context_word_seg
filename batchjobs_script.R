180eead1245a9241910c2547b7343f754662f60e
Rose Maier <rosem@uoregon.edu>
2015-11-10 15:53:48 -0800

    moved evaluation() function to data_processing_functions.r, called par_function(); changed to load contexts r object instead of csv file

--- batchjobs_script.R
+++ batchjobs_script.R
@@ -39,6 +39,18 @@ R CMD BATCH --no-save --no-restore '<%= rscript %>' /dev/stdout
 
 writeLines(simple, "simple.tmpl", sep="\n")
 
+###########################################################
+# get files ready for ACISS
+###########################################################
+save(contexts, file = "contexts")
+#save(radon.1, file = "r_obj/radon1")
+#load(file = "r_obj/radon1")
+# copy utt_orth_phon_KEY.txt to server
+# copy contexts to server (the R file saved above)
+# copy dict_all3_updated.txt to server
+# copy simple.tmpl
+# copy .BatchJobs.R
+
 
 ###########################################################
 # the model (run this in R on ACISS)
@@ -47,75 +59,18 @@ library(BatchJobs)
 getConfig()
 
 # install.packages("dplyr", "tidyr", "doParallel")
-library(dplyr); library(tidyr), library(doParallel); library(devtools)
+library(dplyr); library(tidyr); library(doParallel); library(devtools)
+sessionInfo() # to check
 
-starts <- 1:10
-
-# copy utt_orth_phon_KEY.txt to server
-# copy /Users/TARDIS/Documents/STUDIES/context_word_seg/words by contexts.csv to server
-# copy dict_all3_updated.txt to server
-# copy simple.tmpl
-# copy .BatchJobs.R
+starts <- 1:20
 
 
 batch_function <- function(starts){
   library(dplyr)
   library(tidyr)
   library(devtools)
-  source_url("https://raw.githubusercontent.com/rosemm/context_word_seg/master/data_processing_functions.r")
   
-  evaluation <- function(df, contexts){ # this is the function that should be done in parallel on the 12 cores of each node
-    # contexts <- df[1, c(4:ncol(df))]
-    
-    # pick nontexts
-    results <- nontext_cols(df=df, context_names=colnames(contexts)) # add the nontext col
-    non <- results[[1]]
-    nontexts <- results[[2]]
-    names(nontexts) <- paste("non.", colnames(contexts), sep="")
-    
-    # add nontext columns to dataframe
-    colnames(non) <- colnames(contexts)
-    df.non <- cbind(df[,1:3], non)
-    
-    # expand windows to + - 2 utterances before and after
-    df.non <- expand_windows(df.non)
-    
-    # calculate MIs and TPs
-    nontext.data <- context_results(contexts, df=df.non) # calls make_streams() and calc_MI()
-    
-    # segment speech
-    for(k in 1:length(names(nontext.data))){
-      
-      nontext.data[[k]]$TP85$seg.phon.stream <- segment_speech(cutoff=.85, stat="TP", nontext.data[[k]]$unique.phon.pairs, nontext.data[[k]]$streams$phon.stream)
-      
-      nontext.data[[k]]$MI85$seg.phon.stream <- segment_speech(cutoff=.85, stat="MI", nontext.data[[k]]$unique.phon.pairs, nontext.data[[k]]$streams$phon.stream)
-    }
-    
-    # assess segmentation
-    stat.results <- data.frame(recall=NULL, precision=NULL, stat=NULL, nontext=NULL)
-    for(k in 1:length(names(nontext.data))){
-      
-      nontext.data[[k]]$TP85$seg.results <- assess_seg(seg.phon.stream=nontext.data[[k]]$TP85$seg.phon.stream, words=nontext.data[[k]]$streams$words, dict=dict)
-      
-      TPresults <- colMeans(nontext.data[[k]]$TP85$seg.results[,3:4], na.rm=T)
-      TPresults$stat <- "TP"
-
-      nontext.data[[k]]$MI85$seg.results <- assess_seg(seg.phon.stream=nontext.data[[k]]$MI85$seg.phon.stream, words=nontext.data[[k]]$streams$words, dict=dict)
-      
-      MIresults <- colMeans(nontext.data[[k]]$MI85$seg.results[,3:4], na.rm=T)
-      MIresults$stat <- "MI"
-      
-      this.result <- as.data.frame(rbind(TPresults, MIresults))
-      row.names(this.result) <- NULL
-      this.result$stat <- as.factor(as.character(this.result$stat))
-      this.result$nontext <- names(nontext.data)[[k]]
-      stat.results <- rbind(stat.results,this.result)
-    } 
-    stat.results$nontext <- as.factor(as.character(stat.results$nontext))
-    stat.results$recall <- as.numeric(stat.results$recall)
-    stat.results$precision <- as.numeric(stat.results$precision)
-    return(stat.results)
-  }
+  source_url("https://raw.githubusercontent.com/rosemm/context_word_seg/master/data_processing_functions.r")
   
   # note that key should have the context columns already (from lists, human coding, or topic modeling, etc.)
   key <- read.table("utt_orth_phon_KEY.txt", header=1, sep="\t", stringsAsFactors=F, quote="", comment.char ="")
@@ -126,27 +81,27 @@ batch_function <- function(starts){
   cols <- ncol(dict)
   if(nrow(dict) == 0) stop("dict didn't load")
   
-  contexts <- read.csv("words by contexts.csv")
+  load(file = "contexts")
   if(nrow(contexts) == 0) stop("contexts didn't load")
     
   iter <- 12 # the number of times to generate random samples
 
   library(doParallel)
   registerDoParallel()
-  r <- foreach(1:iter, .combine = rbind) %dopar% evaluation(df, contexts)
+  r <- foreach(1:iter, .combine = rbind) %dopar% par_function(df=df, contexts=contexts)
   # save(r, file="bootstrap_results.data")
 }
 
 
 # create a registry
-id <- "bootstrap"
-reg <- makeRegistry(id = id)
+id <- "bootstrap1"
+reg1 <- makeRegistry(id = id)
 
 # map function and data to jobs and submit
-ids  <- batchMap(reg, batch_function, starts)
-done <- submitJobs(reg, resources = list(nodes = 10, ppn=iter))
+ids  <- batchMap(reg1, batch_function, starts)
+done <- submitJobs(reg1, resources = list(nodes = 10, ppn=iter))
 
-showStatus(reg)
+showStatus(reg1)
 
 results <- data.frame(V1=NULL)
 nodes <- list.files(paste0(id, "-files/jobs"))
