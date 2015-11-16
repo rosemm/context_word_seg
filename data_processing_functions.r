477bfd31f669e9977911e3e5cfc95fad72f114fa
Rose Maier <rosem@uoregon.edu>
2015-11-15 19:26:31 -0800

    added contexts_by_size() for generating "contexts" of different sizes over the whole range of the corpus

--- data_processing_functions.r
+++ data_processing_functions.r
@@ -314,7 +314,21 @@ make_corpus <- function(dist=c("unif", "skewed"), size){ # make an artificial la
   return(corpus)
 }
 
-
+contexts_by_size <- function(key="utt_orth_phon_KEY.txt" , N.sizes){
+  df <- read.table(key, header=1, sep="\t", stringsAsFactors=F, quote="", comment.char ="")
+  start.columns <- ncol(df)
+  
+  # Add columns for each "context", with increasing number of utterances. 
+  # It doesn't matter which utterances are selected for each column since they'll all get shuffled anyway during the bootstrapping process
+  min.utt <- 100
+  max.utt <- nrow(df) - 100 # a sample that includes all of the utterances in the corpus won't have any variability from sample to sample
+  sizes <- round(seq(from=min.utt, to=max.utt, length.out=N.sizes), 0)
+  # add a column to the dataframe for each  corpus size to try
+  for(s in 1:N.sizes){
+    df[[paste0("N.utts", sizes[s])]] <- c( rep(1, sizes[s]), rep(0, nrow(df)-sizes[s]) )
+  }
+  return(df)
+}
 
 check_seed_words <- function(seg.results){
   results <- dplyr::filter(seg.results, context!="none" & !is.na(context))
