bcd1ef6fc354f8172c94dcd36caeb97ed99390d4
Rose Maier <rosem@uoregon.edu>
2015-11-15 19:57:28 -0800

    moved code for generating contexts by size to data_processing_functions.r; wrote code for skewed vs. unif dists

--- simulations.R
+++ simulations.R
@@ -1,20 +1,9 @@
 ####################################################################################
 # how does corpus size affect precision and recall?
 ####################################################################################
-df <- read.table("utt_orth_phon_KEY.txt", header=1, sep="\t", stringsAsFactors=F, quote="", comment.char ="")
 
-start.columns <- ncol(df)
-
-# Add columns for each "context", with increasing number of utterances. 
-# It doesn't matter which utterances are selected for each column since they'll all get shuffled anyway during the bootstrapping process
-N.sizes <- 30 # how many corpus sizes to try
-min.utt <- 100
-max.utt <- nrow(df) - 100 # a sample that includes all of the utterances in the corpus won't have any variability from sample to sample
-sizes <- round(seq(from=min.utt, to=max.utt, length.out=N.sizes), 0)
-# add a column to the dataframe for each  corpus size to try
-for(s in 1:N.sizes){
-  df[[paste0("N.utts", sizes[s])]] <- c( rep(1, sizes[s]), rep(0, nrow(df)-sizes[s]) )
-}
+# make a set of "contexts" from 100 utterances to (nearly) the full size of the corpus
+df <- contexts_by_size(N.sizes=30)
 
 write.table(df, file="contexts_SizeSim.txt", quote=F, col.names=T, row.names=F, append=F, sep="\t")
 # send the above text file to ACISS and use it as the contexts file for a bootstrap analysis
@@ -22,3 +11,22 @@ write.table(df, file="contexts_SizeSim.txt", quote=F, col.names=T, row.names=F, 
 ####################################################################################
 # how does the shape of the distribution affect MI dist, precision, and recall?
 ####################################################################################
+# read in the functions written for this analysis
+source_url("https://raw.githubusercontent.com/rosemm/context_word_seg/master/data_processing_functions.r")
+
+# make a skewed corpus and save to text file
+corpus.skew <- make_corpus(dist="skewed", size=1000)
+write.table(corpus.skew, file="utt_orth_phon_KEY_SKEW.txt", quote=F, col.names=T, row.names=F, append=F, sep="\t")
+# use that corpus to generate a size sim contexts file
+df.skew <- contexts_by_size(read.table("utt_orth_phon_KEY_SKEW.txt", header=1, sep="\t", stringsAsFactors=F, quote="", comment.char ="") , 
+                            N.sizes=30)
+write.table(df.skew, file="contexts_SizeSimSKEW.txt", quote=F, col.names=T, row.names=F, append=F, sep="\t")
+
+
+# make a uniform corpus and save to text file
+corpus.unif <- make_corpus(dist="unif", size=1000)
+write.table(corpus.unif, file="utt_orth_phon_KEY_UNIF.txt", quote=F, col.names=T, row.names=F, append=F, sep="\t")
+# use that corpus to generate a size sim contexts file
+df.unif <- contexts_by_size(read.table("utt_orth_phon_KEY_UNIF.txt", header=1, sep="\t", stringsAsFactors=F, quote="", comment.char =""), 
+                            N.sizes=30)
+write.table(df.skew, file="contexts_SizeSimUNIF.txt", quote=F, col.names=T, row.names=F, append=F, sep="\t")
