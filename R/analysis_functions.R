465d5e77c889a3e21aac0f342e7e5ec678b3b297
Rose Maier <rosemhartman@gmail.com>
2016-04-27 17:21:32 -0700

    added logistic_regression() function

--- R/analysis_functions.R
+++ R/analysis_functions.R
@@ -1,34 +1,3 @@
-# agreement analysis functions
-cat_agreement <- function(cat.codes){
-  
-  stopifnot(length(cat.codes) == 2, require(dplyr), require(tidyr), require(vcd))
-  
-  message("Assessing agreement between ", paste(c(names(cat.codes)[1],names(cat.codes)[2]), collapse=" and "))
-  
-  cat1 <- cat.codes[[1]]
-  cat2 <- cat.codes[[2]]
-  
-  cat1.long <- gather(cat1, key="cat1", value="include", -utt, -orth, -phon) %>% 
-    filter(include==1) %>% 
-    select(-include)
-  cat2.long <- gather(cat2, key="cat2", value="include", -utt, -orth, -phon) %>% 
-    filter(include==1) %>% 
-    select(-include)
-  
-  freqs <- full_join(cat1.long, cat2.long, by="utt")
-  
-  freqs <- freqs %>% 
-    count(cat1, cat2)
-  
-  xt <- xtabs(n ~ cat1 + cat2, data=freqs) # cross-tabs
-  names(attr(xt, "dimnames")) <- c(names(cat.codes)[1],names(cat.codes)[2])
-  
-  chi.sq <- summary(xt) # chi-squared test of independence
-  v <- assocstats(xt) # cramers v
-  
-  return(list(xt, chi.sq, v))
-}
-
 
 threshold_plots <- function(df.model, thresholds, method, save.to, ...){
   
@@ -68,7 +37,7 @@ threshold_plots <- function(df.model, thresholds, method, save.to, ...){
                               labels = c("mean_num_topics", "zero contexts", "one context", "one or two contexts", "two contexts", "more than two contexts"))
   
   method <- toupper(method)
-  additional_args <- as.data.frame(list(...))
+  additional_args <- as.data.frame(list(...), stringsAsFactors=FALSE)
   additional_args <- paste0(colnames(additional_args), additional_args, collapse="_")
   
   ggplot(filter(plot.data, measure=="mean_num_topics"), aes(x=threshold, y=value)) + 
@@ -100,4 +69,81 @@ apply_threshold <- function(df.model, threshold){
     spread(key=topic, value=include)
   
   return(df.bin)
+}
+
+# agreement analysis functions
+cat_agreement <- function(cat.codes){
+  
+  stopifnot(length(cat.codes) == 2, require(dplyr), require(tidyr), require(vcd))
+  
+  message("Assessing agreement between ", paste(c(names(cat.codes)[1],names(cat.codes)[2]), collapse=" and "))
+  
+  cat1 <- cat.codes[[1]]
+  cat2 <- cat.codes[[2]]
+  
+  cat1.long <- gather(cat1, key="cat1", value="include", -utt, -orth, -phon) %>% 
+    filter(include==1) %>% 
+    select(-include)
+  cat2.long <- gather(cat2, key="cat2", value="include", -utt, -orth, -phon) %>% 
+    filter(include==1) %>% 
+    select(-include)
+  
+  freqs <- full_join(cat1.long, cat2.long, by="utt")
+  
+  freqs <- freqs %>% 
+    count(cat1, cat2)
+  
+  xt <- xtabs(n ~ cat1 + cat2, data=freqs) # cross-tabs
+  names(attr(xt, "dimnames")) <- c(names(cat.codes)[1],names(cat.codes)[2])
+  
+  chi.sq <- summary(xt) # chi-squared test of independence
+  v <- assocstats(xt) # cramers v
+  
+  return(list(xt, chi.sq, v))
+}
+
+logistic_regressions <- function(all.methods, outcome_method, predictor_method){
+  stopifnot(require(dplyr), require(tidyr))
+  dvs <- all.methods %>% 
+    select(starts_with(outcome_method)) %>% 
+    as.list()
+
+  predictors <- all.methods %>% 
+    select(starts_with(predictor_method)) %>% 
+    as.matrix()
+    
+  models <- list()
+  summaries <- list()
+  plot.data <- data.frame(est=NULL, se=NULL, method=NULL, context=NULL, outcome=NULL)
+  for(i in 1:length(dvs)){
+    dv <- dvs[[i]]
+    model <- glm(dv ~ predictors - 1,  
+                family=binomial(link = "logit"))
+    # save the models
+    models[[names(dvs)[i]]] <- model
+    # save the model summaries
+    summaries[[names(dvs)[i]]] <- summary(model)
+    # save the coefficient estimates and se's
+    p <- summary(model)$coefficients %>% 
+      as.data.frame() %>% 
+      select(1,2)
+    colnames(p) <- c("est", "se")
+    p$context <- row.names(p)
+    row.names(p) <- NULL
+    if( grepl(x=predictor_method, pattern="STM|LDA") ) { # for topic modeling results
+      p <- extract(p, context, into=c("method", "context"), regex="predictors(.*)[_](topic.*)")
+    } else {
+      p <- extract(p, context, into=c("method", "context"), regex="predictors(.*)[_](.*)")
+    }
+    p$context <- factor(p$context, levels=unique(p$context))
+    p$outcome <- names(dvs)[i]
+    plot.data <- rbind(plot.data, p)
+  }
+  plot <- ggplot(plot.data, aes(y=context, x=est)) +
+    geom_point() +
+    geom_errorbarh(aes(xmin = est-(2*se), xmax = est+(2*se)), height = .2) +
+    facet_wrap(~ outcome, scales = "free_x", nrow=2) +
+    labs(y=NULL, x="Logistic regression coefficients (2SE error bars)" )
+  output <- list(models=models, summaries=summaries, plot.data=plot.data, plot=plot)
+  return(output)
 }
