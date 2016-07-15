fd384819000ca3d7c8b2df7986e7d08012e25fd3
Rose Maier <rosemhartman@gmail.com>
2016-07-14 17:37:57 -0700

    renamed to df_HJ_prop for clarity

--- munge/09-HJ_df.R
+++ munge/09-HJ_df.R
@@ -4,9 +4,10 @@ master_doc_count <- context_cats %>%
   count(utt, category ) %>%
   spread(key=category, value=n, fill = 0) %>%
   separate(col=utt, into=c("file", "UttNum"), sep="_" , remove=F) %>%
-  arrange(file, as.numeric(UttNum) ) 
+  arrange(file, as.numeric(UttNum) ) %>% 
+  select(-file, -UttNum)
 
-df_HJ_raw <- left_join(df, select(master_doc_count, -file, -UttNum), by="utt" ) 
+df_HJ_raw <- left_join(df, master_doc_count, by="utt" ) 
 write.table(df_HJ_raw, file="context_codes/human_judgments/contexts_HJ_raw.txt", quote=F, col.names=T, row.names=F, append=F, sep="\t")
 cache('df_HJ_raw')
 
@@ -18,33 +19,8 @@ master_doc_prop <- master_doc_count %>%
 master_doc_prop$utt <- master_doc_count$utt  # to add back in the non-numeric utt column
 master_doc_prop$total <- NULL  # delete column
 
-df_HJ <- left_join(df, master_doc_prop, by="utt" ) 
-write.table(df_HJ, file="context_codes/human_judgments/contexts_HJ.txt", quote=F, col.names=T, row.names=F, append=F, sep="\t")
-cache('df_HJ')
+df_HJ_prop <- left_join(df, master_doc_prop, by="utt" ) 
+write.table(df_HJ_prop, file=file.path("context_codes", "human_judgments", "contexts_HJ_prop.txt"), quote=F, col.names=T, row.names=F, append=F, sep="\t")
+cache('df_HJ_prop')
 
-votes <- master_doc_count[, 4:ncol( master_doc_count )]
 
-##########################################
-# number of utterances per context
-##########################################
-counts <- sort(colSums(master_doc_count[,4:ncol(master_doc_count)]),decreasing = TRUE)
-count.data <- data.frame(n.utts=counts, 
-                         context=factor(names(counts), levels=names(counts)))
-# over the median number of utterances?
-count.data$over.med <- ifelse(count.data$n.utts > median(counts), "yes", "no") 
-
-ggplot(count.data, aes(x=context, y=n.utts, fill=over.med)) +
-  geom_bar(stat = "identity", show.legend = F) +
-  geom_text(aes(label=n.utts), position=position_dodge(width=0.9), vjust=-0.25)+
-  scale_fill_manual(values = c("yes" = "darkgreen","no" = "grey")) +
-  labs(x=NULL, y="Number of utterances tagged") +
-  theme(axis.text.x = element_text(angle=330, vjust=1, hjust=0)) 
-ggsave("plots/HJ/uttsper_context.png", width=16, height=5, units="in")
-ggplot(count.data, aes(x=context, y=n.utts, fill=over.med)) +
-  geom_bar(stat = "identity", show.legend = F) +
-  geom_text(aes(label=n.utts), position=position_dodge(width=0.9), vjust=-0.25)+
-  scale_fill_manual(values = c("yes" = "darkgreen","no" = "grey")) +
-  labs(x=NULL, y="log(Number of utterances tagged)") +
-  scale_y_log10() +
-  theme(axis.text.x = element_text(angle=330, vjust=1, hjust=0)) 
-ggsave("plots/HJ/uttsper_context_logtrans.png", width=16, height=5, units="in")
