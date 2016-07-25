d93131b4319280a3d06f999478e5b4da2b98770c
Rose Maier <rosemhartman@gmail.com>
2016-07-25 13:04:43 -0700

    adding white space

--- munge/12-TM_thresholding.R
+++ munge/12-TM_thresholding.R
@@ -7,22 +7,28 @@ LDA.threshold <- threshold_plots(df_LDA_prop,
                                  misc.cutoff="none") 
 
 # apply threshold
-df_LDA_bin <- apply_threshold(df_LDA_prop, LDA.threshold, 
-                              plot=T, method="LDA", save.to=file.path("graphs", "LDA"))
+df_LDA_bin <- apply_threshold(df_LDA_prop, 
+                              LDA.threshold, 
+                              plot=TRUE, 
+                              method="LDA", 
+                              save.to=file.path("graphs", "LDA"))
 
 write.table(df_LDA_bin, file=file.path("context_codes", "human_judgments", "contexts_LDA_bin.txt"), quote=F, col.names=T, row.names=F, append=F, sep="\t")
 cache('df_LDA_bin')
 
 # any additional values to be saved in the plot name can be included as extra arguments
 STM.threshold <- threshold_plots(df_STM_prop, 
-                thresholds=seq(0,.75,.05), # what thresholds to try, 
-                method="STM",
-                save.to=file.path("graphs", "STM"),
-                misc.cutoff="none") 
+                                 thresholds=seq(0,.75,.05), # what thresholds to try
+                                 method="STM",
+                                 save.to=file.path("graphs", "STM"),
+                                 misc.cutoff="none") 
 
 # apply threshold
-df_STM_bin <- apply_threshold(df_STM_prop, STM.threshold, 
-                             plot=T, method="STM", save.to=file.path("graphs", "STM"))
+df_STM_bin <- apply_threshold(df_STM_prop, 
+                              STM.threshold, 
+                              plot=TRUE, 
+                              method="STM", 
+                              save.to=file.path("graphs", "STM"))
 
 write.table(df_STM_bin, file=file.path("context_codes", "human_judgments", "contexts_STM_bin.txt"), quote=F, col.names=T, row.names=F, append=F, sep="\t")
 cache('df_STM_bin')
