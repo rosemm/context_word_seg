b9bee631f8f9963405ea5d431745906a7fc51423
Rose Maier <rosemhartman@gmail.com>
2016-07-25 23:20:45 -0700

    sequence plots for visualizing contexts over time

---
+++ src/sequence plots.R
@@ -0,0 +1,13 @@
+WL <- df_WL %>% 
+  mutate_each(funs(ifelse(. > 0, 1, 0)), -utt, -orth, -phon) # for word list analysis only, make all contexts either 1 or 0 (smoothing over 1.5's from expand_windows)
+# remove underscores from WL orth column
+WL$orth <- gsub(x=WL$orth, pattern="_", replacement=" ")
+
+STM.bin <- df_STM_bin
+LDA.bin <- df_LDA_bin
+HJ.bin <- df_HJ_bin
+
+seq_plots(WL, "WL")
+seq_plots(HJ.bin, "HJ")
+seq_plots(LDA.bin, "LDA")
+seq_plots(STM.bin, "STM")
