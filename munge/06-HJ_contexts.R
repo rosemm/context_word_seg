8aa55060054edc567ef802cdf31c9cbaef99ea67
Rose Maier <rosemhartman@gmail.com>
2016-07-27 15:40:55 -0700

    added caching of processed HJ codes (not categories), so re-ran HJ munging

--- munge/06-HJ_contexts.R
+++ munge/06-HJ_contexts.R
@@ -13,11 +13,13 @@
 
 master_doc <- read.table("context_codes/human_judgments/master_doc.txt", header=1, sep="\t", stringsAsFactors=F)
 
-contexts_clean <- master_doc %>% 
+HJ_processedcodes <- master_doc %>% 
   process_codes(min.codes=5, max.codes=5, unique.coders=TRUE) %>% 
   clean_contexts(dir="context_codes/human_judgments", key_file="context_cleaning_keys.txt", interactive = TRUE)
 
-HJ_contexts <- contexts_clean %>% 
+cache('HJ_processedcodes')
+
+HJ_contexts <- HJ_processedcodes %>% 
   clean_categories(dir="context_codes/human_judgments", key_file="categories_cleaning_keys.txt", interactive = TRUE) 
 # note that running clean_categories also updates the .md file, which pushes to here https://github.com/rosemm/context_word_seg/blob/master/categories_keys.md
 
