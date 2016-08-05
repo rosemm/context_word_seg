748ee659ead45fbf0d2310e8510daffdfc280fd1
Rose Maier <rosemhartman@gmail.com>
2016-08-05 08:14:56 -0700

    changed HJ processing of codes to be proportion *across coders* rather than across all codes

--- munge/09-HJ_df.R
+++ munge/09-HJ_df.R
@@ -1,9 +1,12 @@
 
 master_doc_count <- HJ_contexts %>%
   ungroup() %>% 
-  dplyr::select(utt, category) %>%
-  count(utt, category) %>%
+  dplyr::select(utt, category, coder) %>%
+  count(utt, category, coder) %>%
   spread(key=category, value=n, fill = 0) %>%
+  dplyr::select(-coder) %>%
+  group_by(utt) %>% 
+  summarize_all( sum ) %>% 
   separate(col=utt, into=c("file", "UttNum"), sep="_" , remove=FALSE) %>%
   arrange(file, as.numeric(UttNum) ) %>% 
   dplyr::select(-file, -UttNum)
@@ -14,11 +17,7 @@ cache('df_HJ_raw')
 
 master_doc_prop <- master_doc_count %>% 
   ungroup() %>%
-  dplyr::select(-utt) %>%
-  mutate(total=rowSums(., na.rm=FALSE)) %>% 
-  mutate_each(funs(./total)) 
-master_doc_prop$utt <- master_doc_count$utt  # to add back in the non-numeric utt column
-master_doc_prop$total <- NULL  # delete column
+  mutate_at(vars(-utt), funs(./5)) 
 
 df_HJ_prop <- left_join(df, master_doc_prop, by="utt" ) 
 write.table(df_HJ_prop, file=file.path("context_codes", "human_judgments", "contexts_HJ_prop.txt"), quote=F, col.names=T, row.names=F, append=F, sep="\t")
