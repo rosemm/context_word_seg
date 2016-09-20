0d33ee185d2d9b28fc9c34d7604987a067a1a976
Rose Maier <rosemhartman@gmail.com>
2016-09-20 17:39:27 -0400

    fuzzy coding a la Greenacre, 1984

---
+++ lib/fuzzy_code.R
@@ -0,0 +1,12 @@
+#' @export
+fuzzy_code <- function(df_bin){
+  # fuzzy coding a la Greenacre, 1984
+  
+  context_cols <- dplyr::select(df_bin, -utt, -orth, -phon)
+  df_bin$tot <- rowSums(context_cols)
+  df_bin <- as.tbl(df_bin) %>% 
+    mutate(tot=ifelse(tot==0, 1, tot)) %>% # don't want to divide by 0
+    mutate_if(is.numeric, funs(./tot)) %>% 
+    dplyr::select(-tot)
+  return(df_bin)
+}
