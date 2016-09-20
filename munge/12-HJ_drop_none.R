f1425bc5c5a1a23a1cee0e429f6900e1fd4bb307
Rose Maier <rosemhartman@gmail.com>
2016-09-20 17:46:43 -0400

    dropping none context from HJ

---
+++ munge/12-HJ_drop_none.R
@@ -0,0 +1,5 @@
+# drop "none" codes from HJ contexts
+df_HJ_bin <- dplyr::select(df_HJ_bin, -none)
+
+write.table(df_HJ_bin, file=file.path("context_codes", "human_judgments", "contexts_HJ_bin.txt"), quote=F, col.names=T, row.names=F, append=F, sep="\t")
+cache('df_HJ_bin')
