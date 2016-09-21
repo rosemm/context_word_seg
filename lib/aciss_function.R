f5abc62394759391f05748551aa6ea52f1e1ae94
Rose Maier <rosemhartman@gmail.com>
2016-09-21 19:01:25 -0400

    fixed typo in url

--- lib/aciss_function.R
+++ lib/aciss_function.R
@@ -5,7 +5,7 @@ aciss_function <- function(date, id, starts, iter, run_analysis_args, walltime=9
     # devtools::install_github(repo = "rosemm/context_word_seg", subdir = "pkg")
     # library(contextwordseg)
     devtools::source_url("https://raw.githubusercontent.com/rosemm/context_word_seg/master/lib/nontexts_descriptives.R")
-    devtools::source_url("https://raw.githubusercontent.com/rosemm/context_word_seg/master/lib/data_processing_functions.R")
+    devtools::source_url("https://raw.githubusercontent.com/rosemm/context_word_seg/master/lib/data_processing_functions.r")
     devtools::source_url("https://raw.githubusercontent.com/rosemm/context_word_seg/master/lib/utils.R")
     iter <- iter # the number of times to generate random samples
     
