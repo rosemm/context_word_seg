4ebd967403927c09d22dd8d1337fef8f74acf917
Rose Maier <rosemhartman@gmail.com>
2016-09-22 20:47:36 -0400

    function for printing text files to feed to cm

---
+++ lib/print_for_cm.R
@@ -0,0 +1,38 @@
+#' @export
+# prep files for computational models
+print_for_cm <- function(df, nontext, dir = getwd()){
+  methods <- c("WL", "STM", "HJ")
+  
+  if(nontext){
+    # pick nontext utterances
+    non <- nontext_cols(df=df) 
+    # add nontext columns to dataframe
+    df <- cbind(dplyr::select(df, utt, orth, phon), non)
+  }
+  if( !"computational_models" %in% list.files(dir) ){
+    dir.create(file.path(dir, "computational_models"))
+  }
+  for(m in methods){
+    contexts <- dplyr::select(df, starts_with(m))
+    for( k in 1:ncol(contexts) ){
+      phon <- data.frame(phon=df$phon, context=contexts[[k]]) %>% 
+        dplyr::filter(context == 1) %>% 
+        dplyr::select(phon)
+      write.table(phon, 
+                  file = file.path(dir, "computational_models", paste0(colnames(contexts)[k], "_0to1m.phon")),
+                  quote = FALSE,
+                  row.names = FALSE,
+                  col.names = FALSE)
+    }
+  }
+  
+  # MAKE shell script to run Makefiles
+  commands <- paste0("make NAME=", colnames(df)[4:ncol(df)])
+  write(commands, file = file.path(dir, "computational_models", "cm_contexts.sh"))
+}
+
+df <- df_all_cm %>% 
+  select(-phon) %>% 
+  rename(phon = phon_cm)
+
+print_for_cm(df, nontext = TRUE)
