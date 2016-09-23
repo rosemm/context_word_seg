09b06dd4b905499496b8629a0ae005af11dada49
Rose Maier <rosemhartman@gmail.com>
2016-09-22 21:03:12 -0400

    made subdirectory to save to a variable

--- pkg/R/print_for_cm.R
+++ pkg/R/print_for_cm.R
@@ -1,6 +1,6 @@
 #' @export
 # prep files for computational models
-print_for_cm <- function(df, nontext, dir = getwd()){
+print_for_cm <- function(df, nontext, dir = getwd(), save.to="computational_models"){
   methods <- c("WL", "STM", "HJ")
   
   if(nontext){
@@ -10,7 +10,7 @@ print_for_cm <- function(df, nontext, dir = getwd()){
     df <- cbind(dplyr::select(df, utt, orth, phon), non)
   }
   if( !"computational_models" %in% list.files(dir) ){
-    dir.create(file.path(dir, "computational_models"))
+    dir.create(file.path(dir, save.to))
   }
   for(m in methods){
     contexts <- dplyr::select(df, starts_with(m))
@@ -19,7 +19,7 @@ print_for_cm <- function(df, nontext, dir = getwd()){
         dplyr::filter(context == 1) %>% 
         dplyr::select(phon)
       write.table(phon, 
-                  file = file.path(dir, "computational_models", paste0(colnames(contexts)[k], "_0to1m.phon")),
+                  file = file.path(dir, save.to, paste0(colnames(contexts)[k], "_0to1m.phon")),
                   quote = FALSE,
                   row.names = FALSE,
                   col.names = FALSE)
@@ -28,5 +28,5 @@ print_for_cm <- function(df, nontext, dir = getwd()){
   
   # MAKE shell script to run Makefiles
   commands <- paste0("make NAME=", colnames(df)[4:ncol(df)])
-  write(commands, file = file.path(dir, "computational_models", "cm_contexts.sh"))
+  write(commands, file = file.path(dir, save.to, "cm_contexts.sh"))
 }
