87c67cf3c1df10c4a022d41428a78678649b5803
Rose Maier <rosemhartman@gmail.com>
2016-10-13 09:38:52 -0400

    summarizing context estimates more cleanly; handling unclear files for context esimtates in dpseg; adding in ds estimates and ds_Z scores; added binomal test for significant p values

--- munge/18-process_cm_output.R
+++ munge/18-process_cm_output.R
@@ -1,3 +1,19 @@
+############################################################################################
+# Reads in the output from the two computational models (coling and dpseg)
+#
+# First coling (adaptor grammar) then dpseg (HDP).
+# Collects estimates from running the models on contexts and nontext
+# (nontexts are the randomly selected size-matched subcorpora for each context subcorpus).
+# Combines information from both models into one dataframe cm_results.
+# Summarizes cm_results into cm_boot_tests, which includes p values.
+#
+# In order to work, directories must be arranged in the following way:
+# - computational_models directory in the working directory
+# - subdirectories coling_context_output, coling_nontext_output, dpseg_context_output, and dpseg_nontext_output
+# (its fine if there are additional files or directories in computational_models)
+# - output from coling must be saved with each whole Eval folder, with .trscore file inside
+# - output from dpseg must be saved as the .out file 
+############################################################################################
 
 dir <- "computational_models" # the folder where the output folders are 
 
@@ -25,9 +41,15 @@ for(f in 1:length(con_files)){
   }
 }
 context_est_coling <- results_coling %>% 
-  gather(key="measure", value="context_est", token_f.score:boundary_recall) %>% 
+  gather(key="measure", value="value", token_f.score:boundary_recall) %>% 
+  # remove the iter number from the end of the context name
+  extract(context, into="context", regex="([[:alnum:]]+[[:alpha:]])[[:digit:]]*") %>% 
   group_by(context, measure) %>% 
-  summarize(context_est=mean(context_est, na.rm=TRUE)) # if there is more than one estimate for the same context (from running it multiple times), average them
+  # context_est is the mean of all iterations
+  summarize(context_est=mean(value, na.rm=TRUE),
+            context_iters=n(),
+            context_SD=sd(value, na.rm=TRUE),
+            context_SE=context_SD/sqrt(context_iters))  
 
 # read nontext results
 non_files <- file.path(non_dir, list.files(path=non_dir, pattern=".*[.]trscore", recursive = TRUE))
@@ -48,7 +70,8 @@ cm_results_coling <- results_coling %>%
   dplyr::as.tbl() %>% 
   tidyr::extract(col=context, into=c("context", "iter"), regex = "([[:alnum:]]+[[:alpha:]])([[:digit:]]*)") %>% 
   tidyr::gather(key = "measure", value="value", token_f.score:boundary_recall) %>% 
-  dplyr::left_join(context_est_coling, by=c("context", "measure")) %>% # add context results to nontext results
+  # add context results to nontext results
+  dplyr::left_join(context_est_coling, by=c("context", "measure")) %>% 
   tidyr::extract(col=context, into=c("method", "context"), regex="([[:upper:]]{2,3})([[:alnum:]]+)") %>% 
   mutate(model="coling")
 
@@ -67,6 +90,7 @@ contexts <- gsub(x=con_files, pattern = ".*/([[:alnum:]]+)[.]out", replacement =
 
 # read all of the results files and compile into a dataframe with context names
 failed <- NULL
+results_dpseg <- NULL
 for(f in 1:length(con_files)){
   this.result <- readLines(con_files[f])
   this.result <- this.result[grepl(x=this.result, pattern = "^P ")]
@@ -75,22 +99,45 @@ for(f in 1:length(con_files)){
     this.result <- gsub(x=this.result, pattern=" ([[:upper:]]{1,2})", replacement="_\\1")
     # use the _'s to split the result into each of its statistics
     this.result <- stringr::str_split(this.result, pattern="_", simplify=TRUE)
-    measures <- as.character(gsub(x=this.result, pattern="([[:upper:]]{1,2}) .*", replacement = "\\1"))
-    values <- as.numeric(gsub(x=this.result, pattern="[[:upper:]]{1,2} (.*)", replacement = "\\1"))
-    names(values) <- measures
-    this.result <- t(as.data.frame(values))
-    this.result <- cbind(this.result, context=contexts[f])
-    row.names(this.result) <- NULL
-    if(f == 1){
-      results_dpseg <- this.result
+    
+    # every once in a while, the program records two sets of scores
+    # if so, check that the measures are the same, and average
+    for(r in 1:nrow(this.result)){
+      this.measures <- as.character(gsub(x=this.result[r, ], pattern="([[:upper:]]{1,2}) .*", replacement = "\\1"))
+      this.values <- as.numeric(gsub(x=this.result[r, ], pattern="[[:upper:]]{1,2} (.*)", replacement = "\\1"))
+      if(r==1){
+        measures <- this.measures
+        values <- matrix(this.values, nrow=1)
+      } else {
+        measures <- measures[measures %in% this.measures]
+        values <- rbind(values, this.values)
+      }
+    }
+    if(length(measures) == ncol(values)){
+      values <- colMeans(values)
+      names(values) <- measures
+      this.result <- t(as.data.frame(values))
+      this.result <- cbind(this.result, context=contexts[f])
+      row.names(this.result) <- NULL
+      if(f == 1){
+        results_dpseg <- this.result
+      } else {
+        if(colnames(this.result) == colnames(results_dpseg)){
+          results_dpseg <- rbind(results_dpseg, this.result)
+        } else {
+          message("Error: ", contexts[f], "\n", colnames(this.result), "\n", this.result, "\n")
+          failed <- c(failed, f)
+        }
+      }
     } else {
-      results_dpseg <- rbind(results_dpseg, this.result)
+      failed <- c(failed, f)
+      message("Unclear results for ", nontexts[f])
     }
   } else {
     failed <- c(failed, f)
-    message("No results for ", contexts[f])
+    # message("No results for ", nontexts[f])
   }
-} # end of for loop
+}
 message(length(failed), " failed files. ", 100*round(length(failed)/length(contexts), 3), "% of total.")
 
 context_est_dpseg <- results_dpseg %>% 
@@ -101,16 +148,20 @@ context_est_dpseg <- results_dpseg %>%
                 LP, LR, LF, context) %>% 
   # if there are any trailing numbers, drop them
   tidyr::extract(col=context, into=c("context"), regex = "([[:alnum:]]+[[:alpha:]])[[:digit:]]*") %>% 
-  tidyr::gather(key = "measure", value="context_est", token_f.score:LF) %>% 
+  tidyr::gather(key = "measure", value="value", token_f.score:LF) %>% 
   group_by(context, measure) %>% 
-  # if there is more than one estimate for the same context (from running it multiple times), average them
-  summarize(context_est=mean(context_est)) 
+  # context_est is the mean of all iterations
+  summarize(context_est=mean(value, na.rm=TRUE),
+            context_iters=n(),
+            context_SD=sd(value, na.rm=TRUE),
+            context_SE=context_SD/sqrt(context_iters))  
 
 # read nontext results
 non_files <- file.path(non_dir, list.files(path=non_dir, pattern=".*[.]out", recursive = TRUE))
 nontexts <- gsub(x=non_files, pattern = ".*/([[:alnum:]]+)[.]out", replacement = "\\1")
 # read all of the results files and compile into a dataframe with context names
 failed <- NULL
+results_dpseg <- NULL
 for(f in 1:length(non_files)){
   this.result <- readLines(non_files[f])
   this.result <- this.result[grepl(x=this.result, pattern = "^P ")]
@@ -193,15 +244,26 @@ cm_results$context_est <- ifelse(cm_results$context_est < 1, 100*cm_results$cont
 cm_results <- bootstrap_Z(cm_results, value="value", est="context_est", by=c("model", "method", "context", "measure"))
                 
 # add in some information about each context subset
-cm_results <- ds_results %>% 
-  # add in information from nontext descriptives dataframe
+ds_ests <- ds_results %>% 
+  filter(measure != "highest.freq.syl") %>% 
   # dropping the bootstrapped nontext values (we just want the context estimates here)
   dplyr::select(method, context, measure, context_est) %>% 
   unique() %>% 
   # reformat to wide, so each context descriptive measure is a column
-  tidyr::spread(key=measure, value=context_est) %>% 
-  # add these descriptive columns to the cm_results dataframe
-  left_join(cm_results, by=c("method", "context")) %>% 
+  tidyr::spread(key=measure, value=context_est)  
+
+ds_Zs <- ds_results %>% 
+  filter(measure != "highest.freq.syl") %>% 
+  # dropping the bootstrapped nontext values (we just want the context estimates here)
+  dplyr::select(method, context, Z=measure, Z_est) %>% 
+  unique() %>% 
+  # reformat to wide, so each context descriptive measure is a column
+  tidyr::spread(key=Z, value=Z_est, sep="_") 
+
+# add these descriptive columns to the cm_results dataframe
+cm_results <- cm_results %>% 
+  left_join(ds_ests, by=c("method", "context")) %>% 
+  left_join(ds_Zs, by=c("method", "context")) %>% 
   ungroup() %>% 
   dplyr::mutate(model=factor(model, levels=c("coling", "dpseg"), labels=c("Adaptor_Grammar", "HDP")))
 
@@ -210,11 +272,26 @@ cache('cm_results')
 # calculate p vlaues for each context and measure
 cm_boot_tests <- cm_results %>% 
   dplyr::mutate(above=ifelse(value >= context_est, 1, ifelse(value < context_est, 0, NA))) %>% 
-  dplyr::group_by(model, method, context, measure, context_est, Z_est, N.utts, TTR, mean.words.per.utt, prop.one.word.utt) %>% 
+  dplyr::group_by(model, method, context, measure, context_est, Z_est, 
+                  N.utts, 
+                  TTR, mean.words.per.utt, prop.one.word.utt, 
+                  Z_TTR, Z_mean.words.per.utt, Z_prop.one.word.utt) %>% 
   dplyr::summarize(iters = n(), 
-                   p.val = mean(above)) %>% 
+                   n.above = sum(above),
+                   n.below = iters - n.above,
+                   p.hi = n.above/iters,
+                   p.lo = n.below/iters,
+                   p.val = min(p.hi, p.lo),
+                   # is p.val significantly different from alpha? Davidson, R., & MacKinnon, J. G. (2000). Bootstrap tests: How many bootstraps?. Econometric Reviews, 19(1), 55-68.
+                   p.boot.unsure = ifelse(p.hi < p.lo & p.hi < .025, pbinom(n.above, iters, .025),
+                                           ifelse(p.lo < p.hi & p.lo < .025, pbinom(n.below, iters, .025),
+                                                  ifelse(p.hi < p.lo & p.hi >= .025, pbinom(n.above, iters, .025, lower.tail = FALSE),
+                                                         ifelse(p.lo < p.hi & p.lo >= .025, pbinom(n.below, iters, .025, lower.tail = FALSE), NA))))) %>% 
   add_stars()
 cache('cm_boot_tests')
 
+dplyr::filter(cm_boot_tests, p.boot.unsure > .05 & measure == "token_f.score" & N.utts > 100) # these should be run with more bootstrap iterations
+
 hist(dplyr::filter(cm_boot_tests, measure == "token_f.score")$iters, breaks=30)
+hist(dplyr::filter(cm_results, measure == "token_f.score")$context_iters, breaks=30)
 View(cm_boot_tests)
