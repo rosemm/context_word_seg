8150c1175e68b4e41e692bf7445fd418bff64abb
Rose Maier <rosemhartman@gmail.com>
2016-10-18 16:07:39 -0400

    switching LCA to us df_by_method, where each method is a polytomous variable

--- munge/15-LCA_df.R
+++ munge/15-LCA_df.R
@@ -12,42 +12,39 @@
 ###################################################################################################
 
 # context columns should be coded as 0 or 1 at this point
-# turn all context columns into factors if they're not already (poLCA needs factors)
-df_all <- df_all %>% 
-  mutate_if(is.numeric, as.factor) 
-
 method_contexts <- df_all %>% 
   tidyr::gather(key="context", value="value", -utt, -orth, -phon) %>% 
   tidyr::extract(context, into=c("method", "context"), regex="([[:upper:]]+)_(.*)") %>% 
+  # only keep positive context codes (1 means context is occuring, 0 means not)
   dplyr::filter(value == 1) 
 method_counts <- method_contexts %>% 
-  dplyr::count(utt, orth, phon, method) 
+  dplyr::count(utt, orth, phon, method) # returns number of occurrences per method per utterance as n
 df_by_method <- method_counts %>% 
+  # only keeping occurrences where there is exactly one context listed per method
   dplyr::filter(n == 1) %>% 
   dplyr::select(-n) %>% 
-  left_join(method_contexts, by=c("utt", "orth", "phon", "method")) %>% 
+  # join the contexts themselves back in
+  dplyr::left_join(method_contexts, by=c("utt", "orth", "phon", "method")) %>% 
   dplyr::select(-value) %>% 
+  # reformat to wide
   tidyr::spread(key=method, value=context) %>% 
   dplyr::select(utt, orth, phon, HJ, STM, WL) %>% # keep context columns for HJ, STM, and WL
-  mutate_at(vars(HJ, STM, WL), as.factor)
+  dplyr::mutate_at(vars(HJ, STM, WL), as.factor) # turn all context columns into factors if they're not already (poLCA needs factors)
 
-#---------------------------------------------------------------
-# run the LCA models
-
-# f is the formula for us in poLCA commands
-# for LCA with no predictors (just estimating the latent classes), the formula is cbind(all outcome vectors) ~ 1
-all_outcome_vars <- df_all %>% dplyr::select(-utt, -orth, -phon) %>% colnames() %>% paste(collapse = ", ")
-# print all_outcome_vars in the console and then copy it into the formula:
-f <- cbind(WL_bath, WL_bed, WL_body_touch, WL_diaper_dressing, WL_fussing, WL_meal, WL_play, HJ_bathtime, HJ_diaperchange, HJ_dressing, HJ_fussing, HJ_housework, HJ_interaction, HJ_mealtime, HJ_playtime, HJ_sleep, LDA_topic_1, LDA_topic_10, LDA_topic_11, LDA_topic_12, LDA_topic_2, LDA_topic_3, LDA_topic_4, LDA_topic_5, LDA_topic_6, LDA_topic_7, LDA_topic_8, LDA_topic_9, STM_topic_1, STM_topic_10, STM_topic_11, STM_topic_12, STM_topic_2, STM_topic_3, STM_topic_4, STM_topic_5, STM_topic_6, STM_topic_7, STM_topic_8, STM_topic_9) ~ 1
+# f <- cbind(WL_bath, WL_bed, WL_body_touch, WL_diaper_dressing, WL_fussing, WL_meal, WL_play, HJ_bathtime, HJ_diaperchange, HJ_dressing, HJ_fussing, HJ_housework, HJ_interaction, HJ_mealtime, HJ_playtime, HJ_sleep, LDA_topic_1, LDA_topic_10, LDA_topic_11, LDA_topic_12, LDA_topic_2, LDA_topic_3, LDA_topic_4, LDA_topic_5, LDA_topic_6, LDA_topic_7, LDA_topic_8, LDA_topic_9, STM_topic_1, STM_topic_10, STM_topic_11, STM_topic_12, STM_topic_2, STM_topic_3, STM_topic_4, STM_topic_5, STM_topic_6, STM_topic_7, STM_topic_8, STM_topic_9) ~ 1 
 
 library(doParallel)
-registerDoParallel()
-lca_models <- foreach(i=5:6, 
+registerDoParallel() 
+lca_models <- foreach(i=3:29, 
                       .inorder=TRUE,
+                      .errorhandling="pass",
                       .verbose=TRUE,
-                      .packages=c("poLCA", "MASS") ) %dopar% poLCA(f, df_all, nclass=i, maxiter = 10000, nrep=i, na.rm=FALSE)
+                      .packages=c("poLCA", "MASS") ) %dopar% poLCA(cbind(HJ, STM, WL) ~ 1, df_by_method, nclass=i, maxiter = 100000, nrep=i, na.rm=FALSE)
 
-
+save(lca_models, file="lca_models.RData")
+levels(df_by_method$HJ)
+levels(df_by_method$WL)
+levels(df_by_method$STM)
 # cache('lca_models')
 
 #---------------------------------------------------------------
