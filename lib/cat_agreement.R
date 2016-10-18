28e99b0ae2c2a649f3e5b647b8da6ebe34386f43
Rose Maier <rosemhartman@gmail.com>
2016-10-17 22:49:42 -0400

    removed old analyses

--- lib/cat_agreement.R
+++ lib/cat_agreement.R
@@ -2,7 +2,7 @@
 # agreement analysis functions
 cat_agreement <- function(cat.codes, adj=TRUE){
   
-  stopifnot(length(cat.codes) == 2, require(dplyr), require(tidyr), require(vcd))
+  stopifnot(length(cat.codes) == 2, require(dplyr), require(tidyr), require(vcd), require(MRCV))
   
   message("Assessing agreement between ", paste(c(names(cat.codes)[1],names(cat.codes)[2]), collapse=" and "))
   
@@ -10,11 +10,17 @@ cat_agreement <- function(cat.codes, adj=TRUE){
   cat2 <- cat.codes[[2]]
   
   if(adj){
-    adj.chi.sq <- MI.test(data=cbind(dplyr::select(cat1, -utt, -orth, -phon), dplyr::select(cat2, -utt, -orth, -phon)),
+    # tests for simultaneous pairwise marginal independence 
+    # context codes can be multiple response categorical variables (MRCVs), 
+    # where an utterance can be tagged in multiple contexts within a method
+    adj.chi.sq <- MRCV::MI.test(data=cbind(dplyr::select(cat1, -utt, -orth, -phon), dplyr::select(cat2, -utt, -orth, -phon)),
                           I = ncol(cat1)-3, 
                           J = ncol(cat2)-3, 
                           type="boot")
   } else adj.chi.sq <- "Not run"
+  
+  # after running MRCV::MI.test() if desired, remove any utterances tagged for multiple contexts within a method
+  # then analyze with techniques for regular contingency tables(e.g. chi-squared test of independence)
   
   # remove cases that have ambiguous codes within context approach (more than 1 context tagged)
   cat1 <- remove_ambig(cat1)
@@ -29,10 +35,11 @@ cat_agreement <- function(cat.codes, adj=TRUE){
   
   freqs <- full_join(cat1.long, cat2.long, by=c("utt", "orth", "phon"))
   
+  # any utterances with no context tag for a given method are taggged as "none" for that method
   freqs[is.na(freqs)] <- "none"
   
   freqs <- freqs %>% 
-    count(cat1, cat2)
+    dplyr::count(cat1, cat2)
   
   xt <- xtabs(n ~ cat1 + cat2, data=freqs) # cross-tabs
   names(attr(xt, "dimnames")) <- c(names(cat.codes)[1],names(cat.codes)[2])
