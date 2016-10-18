c8efdd64835a9c91d1bd1fc3f73e08782664fb0f
Rose Maier <rosemhartman@gmail.com>
2016-10-17 22:52:03 -0400

    DROP contexts with fewer than 100 utterances; streamlining the data manipulation a bit

--- munge/14-all_df.R
+++ munge/14-all_df.R
@@ -1,31 +1,48 @@
 # combine coding from all methods into one dataframe
 
-WL <- df_WL_bin 
-colnames(WL)[4:ncol(WL)] <- paste0("WL_", colnames(WL)[4:ncol(WL)])
-HJ <- df_HJ_bin
-colnames(HJ)[4:ncol(HJ)] <- paste0("HJ_", colnames(HJ)[4:ncol(HJ)])
-LDA <- df_LDA_bin
-colnames(LDA)[4:ncol(LDA)] <- paste0("LDA_", colnames(LDA)[4:ncol(LDA)])
-STM <- df_STM_bin
-colnames(STM)[4:ncol(STM)] <- paste0("STM_", colnames(STM)[4:ncol(STM)])
+# get context columns from each method (also retain utt column, for matching them up later)
+# for each, append the method abbreviation to the beginning of each context column name
+WL <- df_WL_bin %>% 
+  dplyr::select(-orth, -phon)
+colnames(WL)[2:ncol(WL)] <- paste0("WL_", colnames(WL)[2:ncol(WL)])
+HJ <- df_HJ_bin %>% 
+  dplyr::select(-orth, -phon)
+colnames(HJ)[2:ncol(HJ)] <- paste0("HJ_", colnames(HJ)[2:ncol(HJ)])
+LDA <- df_LDA_bin %>% 
+  dplyr::select(-orth, -phon)
+colnames(LDA)[2:ncol(LDA)] <- paste0("LDA_", colnames(LDA)[2:ncol(LDA)])
+STM <- df_STM_bin %>% 
+  dplyr::select(-orth, -phon)
+colnames(STM)[2:ncol(STM)] <- paste0("STM_", colnames(STM)[2:ncol(STM)])
 
-df_all <- WL %>% 
+df_all <- df %>% 
+  full_join(WL, by=c("utt")) %>% 
   full_join(HJ, by=c("utt")) %>% 
   full_join(LDA, by=c("utt")) %>% 
   full_join(STM, by=c("utt")) %>% 
-  as.tbl() %>% 
-  dplyr::select( -starts_with("orth"), -starts_with("phon")) %>% 
-  left_join(df, by="utt")
+  as.tbl() 
+# remove spaces in column names
+colnames(df_all) <- gsub(x=colnames(df_all), pattern = " ", replacement = "")
+
+# DROP CONTEXTS WITH FEWER THAN 100 UTTERANCES
+all_utts <- df_all %>% 
+  dplyr::select(-utt, -orth, -phon) %>% 
+  colSums(na.rm=TRUE)
+contexts_keep <- names(all_utts[all_utts > 100])
+df_all <- df_all %>% 
+  # one_of matches column names to a list of names in a character vector (in this case, contexts_keep)
+  dplyr::select(utt, orth, phon, one_of(contexts_keep))
 
 cache('df_all')
 
 # REMOVE AMBIGUOUS UTTERANCES
-df_all_noambig <- remove_ambig(WL) %>% 
+df_all_noambig <- df %>% 
+  full_join(remove_ambig(WL), by=c("utt")) %>% 
   full_join(remove_ambig(HJ), by=c("utt")) %>% 
   full_join(remove_ambig(LDA), by=c("utt")) %>% 
   full_join(remove_ambig(STM), by=c("utt")) %>% 
   as.tbl() %>% 
-  dplyr::select( -starts_with("orth"), -starts_with("phon")) %>% 
-  left_join(df, by="utt")
+  # drop contexts with fewer than 100 utterances
+  dplyr::select(utt, orth, phon, one_of(contexts_keep))
 
 cache('df_all_noambig')
