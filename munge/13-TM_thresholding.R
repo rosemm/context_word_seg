000afed91ff1efa765cef9c61fe539be7efa4d50
Rose Maier <rosemhartman@gmail.com>
2016-11-01 14:47:56 -0400

    adding comments

--- munge/13-TM_thresholding.R
+++ munge/13-TM_thresholding.R
@@ -10,27 +10,37 @@
 # one or two contexts per utterance
 ##############################################################
 
-# any additional values to be saved in the plot name can be included as extra arguments
+# ------------------------------------------------------------------
+# LDA
+
+# the function threshold_plots() generates plots showing a range of thresholds
+# the output of the functio is the threshold corresponding to the most utterances with 1 or 2 contexts
+# here that value gets saved as "LDA_threshold"
 LDA.threshold <- threshold_plots(df_LDA_prop, 
                                  thresholds=seq(0,.75,.05), # what thresholds to try, 
-                                 method="LDA",
+                                 method="LDA", # for labeling plots
                                  save.to=file.path("graphs", "LDA"),
                                  misc.cutoff="none") 
+# any additional values to be saved in the plot name can be included as extra arguments
 
 # apply threshold
 df_LDA_bin <- apply_threshold(df_LDA_prop, 
-                              LDA.threshold, 
+                              LDA.threshold, # threshold determined above
                               plot=TRUE, 
-                              method="LDA", 
+                              method="LDA", # for labeling plots
                               save.to=file.path("graphs", "LDA"))
 
 write.table(df_LDA_bin, file=file.path("context_codes", "human_judgments", "contexts_LDA_bin.txt"), quote=F, col.names=T, row.names=F, append=F, sep="\t")
 
+# remove spaces from column names
 colnames(df_LDA_bin) <- gsub(x=colnames(df_LDA_bin), pattern = " ", replacement = "")
 
 cache('df_LDA_bin')
 
-# any additional values to be saved in the plot name can be included as extra arguments
+# ------------------------------------------------------------------
+# STM
+
+# determine threshold
 STM.threshold <- threshold_plots(df_STM_prop, 
                                  thresholds=seq(0,.75,.05), # what thresholds to try
                                  method="STM",
