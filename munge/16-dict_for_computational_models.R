a58ba879481c55ac3a6ecf5e03487329f49239d7
Rose Maier <rosemhartman@gmail.com>
2016-11-01 15:16:01 -0400

    adding comments

--- munge/16-dict_for_computational_models.R
+++ munge/16-dict_for_computational_models.R
@@ -1,22 +1,42 @@
+####################################################################################
+# The adaptor grammar software expects different formatting of the phon stream than dpseg.
+# The original files here use Swingley's (2005) dictionary, based on CELEX pronunciations,
+# but py-cfg (the adaptor grammar) expects DARPABET pronunciations.
+# Rather than using potentially different pronunciations read from the DARPABET coding, 
+# the existing phonetic approximations are simply reformatted to match DARPABET formatting.
+# This means the "phonemes" that are the basis for the analysis for the adaptor grammar
+# exactly correspond the ones that are used for the dpseg model. 
+# It also means that the pronunciations depicted here may not line up with standard DARPABET
+# coding (i.e. one could not rely on these pronunciations and the DARPABET dictionary to 
+# regenerate the original orth stream).
+####################################################################################
+
+# the phoneSet file provided in the Boerschinger (2012) materials for implementing apdaptor grammars
+# converts single character phonemes into the characters available for py-cfg
 coling_dict <- read.table("doc/coling_phoneSet.txt")
 colnames(coling_dict) <- c("output", "input")
-
-# dict_syls <- unique(strsplit(paste(dict$phon, collapse = "-"), split = "-")[[1]])
 
 # have to remove stress information to look at it by phoneme (stress doesn't apply to phons)
 dict_phons <- gsub(x=dict$phon, pattern = "'", replacement = "", fixed = TRUE)
 dict_phons <- gsub(x=dict_phons, pattern = "\"", replacement = "", fixed = FALSE)
 dict_phons <- gsub(x=dict_phons, pattern = "-", replacement = "", fixed = TRUE)  
-dict_phons <- unique(strsplit(paste(dict_phons, collapse = ""), split = "")[[1]])
+dict_phons <- unique(strsplit(paste(dict_phons, collapse = ""), split = "")[[1]]) # all phonemes used in this corpus
 
+# the current phonemes that have translations ready
 okay <- dict_phons[dict_phons %in% coling_dict$input]
+# the current phonemes that do NOT have translations ready
 unknown <- dict_phons[!dict_phons %in% coling_dict$input]
+# the translations that are NOT going to be used
 unused <- coling_dict[!coling_dict$input %in% dict_phons, ]
+# the translations that ARE going to be used
 use <- coling_dict[coling_dict$input %in% dict_phons, ]
 
+# a set of additional translations that do not conflict with the existing ones
+# to cover the unknown phonemes in this corpus
 add <- read.table("doc/coling_phoneSet_add.txt", comment.char ="", quote = "")
 colnames(add) <- c("output", "input")
 
+# combined translations (will cover all phonemes in corpus now)
 coling_dict <- rbind(use, add) %>% 
   dplyr::arrange(output)
 
@@ -25,7 +45,7 @@ length(dict_phons[!dict_phons %in% coling_dict$input]) == 0 # there should be no
 write.table(coling_dict, "phoneSet_updated.txt", sep = "\t")
 cache('coling_dict')
 
-# Word boundaries marked by tab in phon_cm, phones separated by space
+# for py-cfg: word boundaries marked by tab, phones separated by space
 
 # in this file, phon has words separated by space and syllables within words separated by -
 phon_cm_d <- read.table("utt_orth_phon_KEY.txt", sep="\t", quote="", comment.char ="", header=1, stringsAsFactors=F)%>% 
